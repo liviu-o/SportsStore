a47c60867d51751be3010a317b01ed01f5557517
LiviuCauia <li.viu@mail.com>
2023-01-28 15:52:29 +0000

    Add order checkout and tests

--- SportsStore/Models/StroreDbContext.cs
+++ SportsStore/Models/StroreDbContext.cs
@@ -2,8 +2,10 @@
 
 namespace SportsStore.Models
 {
-    public class StoreDbContext : DbContext {
-        public StoreDbContext(DbContextOptions<StoreDbContext> options): base(options) { }
+    public class StoreDbContext : DbContext
+    {
+        public StoreDbContext(DbContextOptions<StoreDbContext> options) : base(options) { }
         public DbSet<Product> Products => Set<Product>();
+        public DbSet<Order> Orders => Set<Order>();
     }
 }
